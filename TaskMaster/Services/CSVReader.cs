eca1533b644565ed2ecb60034895f0b8a725ca84
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2024-04-12 21:09:49 +0200

    небольшие изменения

---
+++ TaskMaster/Services/CSVReader.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMaster.Models;
+
+namespace TaskMaster.Services
+{
+    public static class CSVReader
+    {
+        public static void InfoTotal(ref List<Models.Task> tasks, ref List<User> users, StreamReader stream)
+        {
+            //var dataReader = new StreamReader(stream);
+
+            string[] temp = stream.ReadToEnd().Split('^');
+
+            //StreamReader tasksInfo = new StreamReader(temp[0]);
+            //StreamReader userInfo = new StreamReader (temp[1]);
+
+            TextReader tasksReader = new StringReader(temp[1]);
+            TextReader usersReader = new StringReader(temp[0]);
+
+            CsvConfiguration csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+
+            CsvReader reader = new CsvReader(tasksReader, csvConfig);
+            tasks = reader.GetRecords<Models.Task>().ToList();
+
+            reader = new CsvReader(usersReader, csvConfig);
+            users = reader.GetRecords<User>().ToList();
+
+            tasksReader.Close();
+            usersReader.Close();
+
+            stream.Close();
+
+            //dataReader.Close();
+        }
+    }
+}
---
+++ TaskMaster/Services/HttpWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMaster.Services
+{
+    public class HttpWork
+    {
+        private const string old_url = @"http://192.168.10.106:8080/?response=auth&login={login}&pass={password}";
+        //private string data_url = string.Empty;
+        private static HttpClient httpClient = new HttpClient();
+
+        public async Task<Stream> GetDataStream(string login, string password)
+        {
+            var data_url = await httpClient.GetAsync($"http://176.123.160.24:8080/?response=auth&login={login}&pass={password}");
+            /*var response = await httpClient.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);*/ // возвращается только заголовок ответа,
+            //а пока еще не его тело 
+            var content = await data_url.Content.ReadAsStringAsync();
+            return await data_url.Content.ReadAsStreamAsync();
+        }
+    }
+}
